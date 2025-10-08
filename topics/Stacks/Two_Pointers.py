815de5beea05b93385935bfb11274123d5065b44
Carter <145146370+Debugger3000@users.noreply.github.com>
2025-10-07 21:12:26 -0400

    Create Two_Pointers.py

---
+++ topics/Stacks/Two_Pointers.py
@@ -0,0 +1,25 @@
+# classic paldindrome, not much to say here
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # remove non alpha characters
+        clean = ''.join(ch.lower() for ch in s if ch.isalnum())
+       
+
+        if len(clean) == 1:
+            return True
+
+        # palindromes cannot be odd
+        mid = len(clean) // 2
+        p1 = 0
+        p2 = 0
+        for i in range(mid):
+            p1 = i
+            p2 = -(i+1)
+            
+            if(clean[p1] != clean[p2]):
+                return False
+            if i == mid:
+                return True
+        return True
+            
